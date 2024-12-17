993bf8cb74f8b57646007cfa4ba0a03752bf8e9b
김승찬 <s24041@gsm.hs.kr>
2024-12-17 10:26:34 +0900

    init

---
+++ gistory/src/main/java/gsm/gistory/GistoryApplication.java
@@ -0,0 +1,13 @@
+package gsm.gistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GistoryApplication.class, args);
+	}
+
+}
---
+++ gistory/src/test/java/gsm/gistory/GistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package gsm.gistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
