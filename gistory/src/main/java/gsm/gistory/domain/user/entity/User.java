2c1d097df2eb0f1661a03a8fd78ad42a476251f0
김승찬 <s24041@gsm.hs.kr>
2024-12-18 14:25:27 +0900

    feat: user entity 설정

---
+++ gistory/src/main/java/gsm/gistory/domain/user/entity/User.java
@@ -0,0 +1,33 @@
+package gsm.gistory.domain.user.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@gsm\\.hs\\.kr$",
+            message = "이메일은 @gsm.hs.kr 도메인으로 끝나야 합니다."
+    )
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String name;
+}
+
