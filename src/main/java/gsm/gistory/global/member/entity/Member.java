b6ba109a885959d8ecfd38a8eb89df0f9f5776dd
김승찬 <s24041@gsm.hs.kr>
2024-12-24 11:53:37 +0900

    feat: 회원관련로직 추가

---
+++ src/main/java/gsm/gistory/global/member/entity/Member.java
@@ -0,0 +1,37 @@
+package gsm.gistory.global.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@Entity
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @NotBlank
+    @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message="이메일 주소 양식을 확인해주세요")
+    @Column(length = 100, unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String profilePath;
+
+    public void updateProfilePath(String profilePath) {
+        this.profilePath = profilePath;
+    }
+
+    public Member(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
