4c3fd57467f59a91acb3ca6d9dbd402b02dedb2e
김승찬 <s24041@gsm.hs.kr>
2024-12-24 11:56:45 +0900

    delete: auth 다 갈아엎기 ㅠ

--- src/main/java/gsm/gistory/domain/auth/service/SignUpService.java
+++
@@ -1,9 +0,0 @@
-package gsm.gistory.domain.auth.service;
-
-import gsm.gistory.domain.auth.dto.request.SignUpRequest;
-import gsm.gistory.domain.auth.dto.response.SignUpResponse;
-
-public interface SignUpService {
-    SignUpResponse signUp(SignUpRequest request);
-}
-
