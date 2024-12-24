6ddaa16bf6ea19f865abe522b1514436283c09eb
김승찬 <s24041@gsm.hs.kr>
2024-12-24 11:57:22 +0900

    delete: auth 다 갈아엎기 ㅠ

--- src/main/java/gsm/gistory/domain/auth/service/LogoutService.java
+++
@@ -1,8 +0,0 @@
-package gsm.gistory.domain.auth.service;
-
-import gsm.gistory.domain.auth.dto.response.LogoutResponse;
-
-public interface LogoutService {
-    LogoutResponse logout(String token);
-}
-
