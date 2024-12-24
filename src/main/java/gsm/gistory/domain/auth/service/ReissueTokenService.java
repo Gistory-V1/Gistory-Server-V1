73359c59523d8f87d589416539c4c03bf41c1f12
김승찬 <s24041@gsm.hs.kr>
2024-12-24 11:56:55 +0900

    delete: auth 다 갈아엎기 ㅠ

--- src/main/java/gsm/gistory/domain/auth/service/ReissueTokenService.java
+++
@@ -1,8 +0,0 @@
-package gsm.gistory.domain.auth.service;
-
-import gsm.gistory.domain.auth.dto.response.ReissueResponse;
-
-public interface ReissueTokenService {
-    ReissueResponse execute(String refreshToken);
-}
-
