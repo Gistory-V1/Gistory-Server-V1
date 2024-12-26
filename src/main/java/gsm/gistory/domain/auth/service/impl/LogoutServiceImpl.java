67675d72c1e6e2721c4dcf07f61964e354c06f03
김승찬 <s24041@gsm.hs.kr>
2024-12-26 09:17:05 +0900

    feat: auth 관련 serviceimpl 로직 설정

---
+++ src/main/java/gsm/gistory/domain/auth/service/impl/LogoutServiceImpl.java
@@ -0,0 +1,30 @@
+package gsm.gistory.domain.auth.service.impl;
+
+import gsm.gistory.domain.auth.dto.response.LogoutResponse;
+import gsm.gistory.domain.auth.service.LogoutService;
+import gsm.gistory.global.security.jwt.JwtTokenProvider;
+import gsm.gistory.global.security.jwt.exception.CustomException;
+import gsm.gistory.global.security.jwt.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LogoutServiceImpl implements LogoutService {
+
+    private final JwtTokenProvider jwtProvider;
+
+    @Override
+    public LogoutResponse logout(String token) {
+        if (!token.startsWith("Bearer ")) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+        String accessToken = token.substring(7);
+
+        if (!jwtProvider.validateToken(accessToken)) {
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+
+        return new LogoutResponse("로그아웃 성공");
+    }
+}
