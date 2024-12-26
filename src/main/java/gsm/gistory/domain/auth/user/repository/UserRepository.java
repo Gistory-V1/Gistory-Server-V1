366c1601df70804b97327c6005af2f265de555c1
김승찬 <s24041@gsm.hs.kr>
2024-12-26 09:16:46 +0900

    feat: auth 관련 service 인터페이스 설정

---
+++ src/main/java/gsm/gistory/domain/auth/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package gsm.gistory.domain.auth.user.repository;
+
+import gsm.gistory.domain.auth.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
+
