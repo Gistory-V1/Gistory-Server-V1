7b800c31b593e55c1dfbc24ce6d7e5c50824fd22
김승찬 <s24041@gsm.hs.kr>
2024-12-26 09:14:00 +0900

    feat: auth 관련 service 인터페이스 설정

---
+++ src/main/java/gsm/gistory/domain/auth/service/LoginService.java
@@ -0,0 +1,8 @@
+package gsm.gistory.domain.auth.service;
+
+import gsm.gistory.domain.auth.dto.request.LoginRequest;
+import gsm.gistory.domain.auth.dto.response.LoginResponse;
+
+public interface LoginService {
+    LoginResponse login(LoginRequest loginRequest);
+}
