f5d54abb1af6df5f4f21c0d81cebf1e63c28b1ed
김승찬 <s24041@gsm.hs.kr>
2024-12-26 20:43:34 +0900

    feat: errorcode 추가

--- src/main/java/gsm/gistory/global/security/jwt/exception/ErrorCode.java
+++ src/main/java/gsm/gistory/global/security/jwt/exception/ErrorCode.java
@@ -12,9 +12,10 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."); // 추가
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    INVALID_EMAIL(HttpStatus.BAD_REQUEST, "유효하지 않은 이메일 형식입니다.");
+
 
     private final HttpStatus status;
     private final String message;
 }
-
