9fa44bf470fc277b448a86c7ed3f3e8821e3b347
김승찬 <s24041@gsm.hs.kr>
2024-12-29 00:48:21 +0900

    feat: top5 관련 코드 추가

--- src/main/java/gsm/gistory/domain/post/repository/PostRepository.java
+++ src/main/java/gsm/gistory/domain/post/repository/PostRepository.java
@@ -2,6 +2,11 @@ package gsm.gistory.domain.post.repository;
 
 import gsm.gistory.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT p FROM Post p ORDER BY p.views DESC")
+    List<Post> findTop5ByViews();
 }
