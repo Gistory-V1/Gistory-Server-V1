e5c38dfde5bcbf368a4d9dabec89301e20839c5a
김승찬 <s24041@gsm.hs.kr>
2024-12-28 22:04:43 +0900

    feat: Post 엔티티 추가

---
+++ src/main/java/gsm/gistory/domain/post/entity/Post.java
@@ -0,0 +1,42 @@
+package gsm.gistory.domain.post.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "post")
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String email;
+
+    @Column(nullable = false)
+    private Long likeCount = 0L;
+
+    @Column(nullable = false)
+    private Long views = 0L;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public void incrementViews() {
+        this.views += 1;
+    }
+}
