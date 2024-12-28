2359e8670f7bd1ff8ccadbede6eb5502e0f20601
김승찬 <s24041@gsm.hs.kr>
2024-12-29 00:48:24 +0900

    feat: top5 관련 코드 추가

--- src/main/java/gsm/gistory/domain/profile/repository/ProfileRepository.java
+++ src/main/java/gsm/gistory/domain/profile/repository/ProfileRepository.java
@@ -2,9 +2,14 @@ package gsm.gistory.domain.profile.repository;
 
 import gsm.gistory.domain.profile.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Optional<Profile> findByName(String name);
+
+    @Query("SELECT p FROM Profile p ORDER BY p.subCount DESC")
+    List<Profile> findTop5BySubCount();
 }
