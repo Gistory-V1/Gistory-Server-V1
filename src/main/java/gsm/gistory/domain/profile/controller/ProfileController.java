ab144ee4f0ba5716b652e90fb2964ef0a7db0da5
김승찬 <s24041@gsm.hs.kr>
2024-12-30 04:25:13 +0900

    style: 프로필 조회 controller 수정

--- src/main/java/gsm/gistory/domain/profile/controller/ProfileController.java
+++ src/main/java/gsm/gistory/domain/profile/controller/ProfileController.java
@@ -5,10 +5,7 @@ import gsm.gistory.domain.profile.service.ProfileService;
 import gsm.gistory.global.security.jwt.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +15,7 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @GetMapping
-    public ResponseEntity<Object> getProfile(@RequestHeader("name") String name) {
+    public ResponseEntity<Object> getProfile(@RequestParam("name") String name) {
         try {
             ProfileResponse response = profileService.getProfile(name);
             return ResponseEntity.ok(response);
