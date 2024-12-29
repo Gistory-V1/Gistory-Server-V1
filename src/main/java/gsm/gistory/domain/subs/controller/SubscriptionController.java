ad2442395b5e800a0e0d014e8c87b607e279c6ed
김승찬 <s24041@gsm.hs.kr>
2024-12-30 02:55:09 +0900

    style: 요청 endpoint 수정

--- src/main/java/gsm/gistory/domain/subs/controller/SubscriptionController.java
+++ src/main/java/gsm/gistory/domain/subs/controller/SubscriptionController.java
@@ -31,7 +31,7 @@ public class SubscriptionController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/cancel")
+    @PostMapping
     public ResponseEntity<UnsubscribeResponse> unsubscribe(
             @RequestHeader("Authorization") String authorization,
             @RequestBody SubscribeRequest request) {
