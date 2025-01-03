570c21b748eb6b0602fa21f5ffbf7cf2a2126cf1
김승찬 <s24041@gsm.hs.kr>
2025-01-03 22:49:45 +0900

    style: 개행 추가, 삭제 및 불필요 주석 제거

--- src/main/java/gsm/gistory/domain/auth/dto/response/ReissueTokenResponse.java
+++ src/main/java/gsm/gistory/domain/auth/dto/response/ReissueTokenResponse.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReissueTokenResponse {
-
     private String accessToken;
     private String accessTokenExpiresIn;
 }
